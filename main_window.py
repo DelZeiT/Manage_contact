a833311f0c4aa0ea45a49cf308fa7075617eaa48
DelZeiT <maksimcvetkov1106@gmail.com>
2023-07-26 19:15:10 +0300

    TIC-001: добавил функцию открывания окна для добавления контакта.

--- main_window.py
+++ main_window.py
@@ -1,6 +1,6 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
-#from add_window_py import AddWindow
+# from add_window_py import AddWindow
 
 Form, Window = uic.loadUiType("main_window_ui.ui")
 
@@ -9,6 +9,14 @@ window = Window()
 form = Form()
 form.setupUi(window)
 window.show()
+
+
+# функция открывания окна для добавления контакта
+def open_add_window():
+    pass
+
+
+form.pusnButton.clicked.connect(open_add_window)
 
 
 app.exec()
