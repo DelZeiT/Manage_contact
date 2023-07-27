e0483a0ecf381c954549ee30506c9ce7ae015a99
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-07-27 06:58:19 +0300

    TIK-002: теперь работает ввод с интерфейса данных

--- add_wind.py
+++ add_wind.py
@@ -2,17 +2,22 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from backend_sql import Info_Contact
 
+# функция обработки всех кнопок в окне добавления контакта
 def open_add_window():
     Form2, Window2 = uic.loadUiType("add_window_ui.ui")
     window2 = Window2()
     form2 = Form2()
     form2.setupUi(window2)
 
+
     #кнопка добавление контакта
     def press_add_contact():
-        info_contact = Info_Contact('3', 'Вася', '8999000', 'vasya@gmail.com')
+        text_LName = form2.lineEdit.text()
+        text_FName = form2.lineEdit_2.text()
+        text_Number = form2.lineEdit_3.text()
+        text_Email = form2.lineEdit_4.text()
+        info_contact = Info_Contact(text_LName, text_FName, text_Number, text_Email)
         info_contact.sql_query()
-
 
     #кнопка отмены
     def press_cancel():
