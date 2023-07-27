e66b5b2053c8d779826ef5f30ad0d42cf4f2f1ab
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-07-27 05:00:05 +0300

    TIK-002: сделал начальный функционал sql

--- backend_sql.py
+++ backend_sql.py
@@ -1,1 +1,42 @@
-import pyodbc
+import sqlite3 as sql3
+
+# если база уже есть, то она просто откроется, иначе создастся
+connection = sql3.connect('Contacts.db')
+cursor = connection.cursor() # для выполнения sql запросов
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS contact (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                                      LName VARCHAR(250), 
+                                                      FName VARCHAR(250), 
+                                                      Number INTEGER,  
+                                                      Email VARCHAR(250))''')
+
+cursor.execute('''INSERT INTO contact (LName, FName, Number, Email) VALUES (?,?,?,?)''', ('Пупкин', 'Вася', 8999000, 'vasya@gmail.com'))
+
+connection.commit()
+
+
+
+cursor.execute('''SELECT * FROM contact''')
+rows = cursor.fetchall()
+for row in rows:
+    print(*row)
+
+
+connection.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
