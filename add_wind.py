e786270f903daef535c095916d7e638e1666aece
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-07-27 06:40:48 +0300

    TIK-002: в файле add_win - созданы обработчики кнопок для add_window в файле backend_sql - создан класс, который принимает данные о клиенте, распаковывает кортеж и отправляет его пока что на консоль

--- add_wind.py
+++ add_wind.py
@@ -1,7 +1,28 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from backend_sql import Info_Contact
+
+def open_add_window():
+    Form2, Window2 = uic.loadUiType("add_window_ui.ui")
+    window2 = Window2()
+    form2 = Form2()
+    form2.setupUi(window2)
+
+    #кнопка добавление контакта
+    def press_add_contact():
+        info_contact = Info_Contact('3', 'Вася', '8999000', 'vasya@gmail.com')
+        info_contact.sql_query()
 
 
+    #кнопка отмены
+    def press_cancel():
+        window2.close()
 
+    form2.pushButton.clicked.connect(press_add_contact)
+    form2.pushButton_2.clicked.connect(press_cancel)
 
+    window2.show()
+    window2.exec_()
 
 
 
