17e97f094fc4e42abcb96bf31706f6f786aa17bd
DelZeiT <maksimcvetkov1106@gmail.com>
2023-07-28 17:59:16 +0300

    TIC-001: добавил добавление кнопок после нажатия кнопки "Поиск" (тест), заменил файл main_window_ui.py на обновленную версию с ListWidget.

--- main_window.py
+++ main_window.py
@@ -1,8 +1,9 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 from add_wind import open_add_window
 
-Form, Window = uic.loadUiType("main_window_ui.ui")
+Form, Window = uic.loadUiType("main_window_ui2.ui")
 
 app = QApplication([])
 window = Window()
@@ -18,6 +19,21 @@ def start_add_window():
 
 form.pushButton.clicked.connect(open_add_window)
 
+
+def add_new_button():
+    list_widget = form.listWidget
+    new_button = QPushButton("Новая кнопка")
+    new_button.setFixedSize(120, 40)
+
+    new_button.setStyleSheet("text-align: left; padding-left: 10px;")
+
+    list_widget_item = QListWidgetItem()
+    list_widget_item.setSizeHint(new_button.sizeHint())
+    list_widget.addItem(list_widget_item)
+    list_widget.setItemWidget(list_widget_item, new_button)
+
+
+form.pushButton_3.clicked.connect(add_new_button)
 
 # функция открывания окна для удаления контакта
 def open_delete_window():
