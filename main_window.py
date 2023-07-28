0c156c9332b7a9636dd3553ff624cfa9669ecc18
DelZeiT <maksimcvetkov1106@gmail.com>
2023-07-28 20:39:20 +0300

    TIC-001: починил прокрутку элементов.

--- main_window.py
+++ main_window.py
@@ -2,13 +2,12 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from add_wind import open_add_window
 
-Form, Window = uic.loadUiType("main_window_ui2.ui")
+Form, Window = uic.loadUiType("main_window_ui3.ui")
 
 app = QApplication([])
 window = Window()
 form = Form()
 form.setupUi(window)
-window.show()
 
 
 # функция открывания окна для добавления контакта.
@@ -37,5 +36,6 @@ def open_delete_window():
 
 form.pushButton_2.clicked.connect(open_delete_window)
 
-
+window.show()
 app.exec()
+
