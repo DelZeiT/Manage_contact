2e1f086a5067acf496683c4e08bcabab5d7b2a52
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-07-30 01:47:04 +0300

    TIK-004: .

--- contact_wind.py
+++ contact_wind.py
@@ -1,28 +1,7 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import *
-from refactor_contact_wind import refactor_contact
+# from PyQt5 import uic
+# from PyQt5.QtWidgets import *
+# from refactor_contact_wind import refactor_contact
 
 
 
-# открывает окно контакт
-def open_contact_window():
-    Form4, Window4 = uic.loadUiType("contact_window_ui.ui")
-    window4 = Window4()
-    form4 = Form4()
-    form4.setupUi(window4)
-
-    # переменные с инфой о пользователе
-    # form4.lineEdit_2.setText(lname)
-    # form4.lineEdit.setText(fname)
-    # form4.lineEdit_3.setText(number)
-    # form4.lineEdit_4.setText(email)
-
-
-    # редактор контакта
-    def open_window_in_contact():
-       refactor_contact()
-    form4.pushButton.clicked.connect(open_window_in_contact)
-
-
-    window4.exec_()
 
--- main_window.py
+++ main_window.py
@@ -1,6 +1,8 @@
 from backend_sql import *
 from contact_wind import *
-
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from refactor_contact_wind import refactor_contact
 
 Form, Window = uic.loadUiType("main_window_ui3.ui")
 
@@ -9,6 +11,10 @@ window = Window()
 form = Form()
 form.setupUi(window)
 
+# global_LName = None
+# global_FName = None
+# global_Number = ''
+# global_Email = None
 
 
 # функция обработки всех кнопок в окне добавления контакта
@@ -18,42 +24,78 @@ def open_add_window():
     form2 = Form2()
     form2.setupUi(window2)
 
-
-    #кнопка добавление контакта
+    # кнопка добавление контакта
     def press_add_contact():
-        text_LName = form2.lineEdit.text() # имя
-        text_FName = form2.lineEdit_2.text() # фамилия
-        text_Number = form2.lineEdit_4.text() # номер
-        text_Email = form2.lineEdit_3.text() # почта
+        text_LName = form2.lineEdit.text()  # имя
+        text_FName = form2.lineEdit_2.text()  # фамилия
+        text_Number = form2.lineEdit_4.text()  # номер
+        text_Email = form2.lineEdit_3.text()  # почта
+
+        # ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
+        # global global_LName
+        # global_LName = text_LName
+        # global global_FName
+        # global_FName = text_FName
+        # global global_Number
+        # global_Number = text_Number
+        # global global_Email
+        # global_Email = text_Email
 
         if not text_LName or not text_FName or not text_Number or not text_Email:
             QMessageBox.warning(window2, 'Ошибка!', 'Заполните все поля.', QMessageBox.Ok)
             return
 
-        info_contact = InfoContact(text_LName, text_FName, text_Number, text_Email) # отправка в класс SQL
+        info_contact = InfoContact(text_LName, text_FName, text_Number, text_Email)  # отправка в класс SQL
         info_contact.sql_query()
 
         lname_cl = GetDataContact('LName', text_Number)
         fname_cl = GetDataContact('FName', text_Number)
         number_cl = GetDataContact('Number', text_Number)
         email_cl = GetDataContact('Email', text_Number)
-        add_new_button(*lname_cl.get_last_name(), *fname_cl.get_last_name(), *number_cl.get_last_name())
+        add_new_button(*lname_cl.get_another_name(), *fname_cl.get_another_name(), *number_cl.get_another_name())
 
-        window2.close()
+        return info_cont(text_Number, text_LName, text_FName, text_Email)
 
-
-    #кнопка отмены
+    # кнопка отмены
     def press_cancel():
         window2.close()
 
-
-    form2.pushButton.clicked.connect(press_add_contact) # кнопка добавить
-    form2.pushButton_2.clicked.connect(press_cancel) # кнопка отмены
+    form2.pushButton.clicked.connect(press_add_contact)  # кнопка добавить
+    form2.pushButton_2.clicked.connect(press_cancel)  # кнопка отмены
 
     window2.show()
     window2.exec_()
 
 
+# открывает окно контакт
+def open_contact_window():
+    Form4, Window4 = uic.loadUiType("contact_window_ui.ui")
+    window4 = Window4()
+    form4 = Form4()
+    form4.setupUi(window4)
+
+
+
+    # редактор контакта
+    def open_window_in_contact():
+        refactor_contact()
+
+    form4.pushButton.clicked.connect(open_window_in_contact)
+
+    window4.exec_()
+
+
+def info_cont(text_Number, text_LName, text_FName, text_Email):
+    Form4, Window4 = uic.loadUiType("contact_window_ui.ui")
+    window4 = Window4()
+    form4 = Form4()
+    form4.setupUi(window4)
+
+    # переменные с инфой о пользователе
+    form4.lineEdit_surname.insert(*GetDataContact(text_LName, text_Number).get_Lname())
+    form4.lineEdit_name.insert(*GetDataContact(text_FName, text_Number).get_Fname())
+    form4.lineEdit_number.insert(*GetDataContact(text_Number, text_Number).get_Number())
+    form4.lineEdit_mail.insert(*GetDataContact(text_Email, text_Number).get_Email())
 
 
 # функция добавления контакта в лист
@@ -92,7 +134,5 @@ form.pushButton.clicked.connect(open_add_window)
 # form.pushButton_3.clicked.connect(add_new_button)
 form.pushButton_2.clicked.connect(open_delete_window)
 
-
 window.show()
 app.exec()
-
